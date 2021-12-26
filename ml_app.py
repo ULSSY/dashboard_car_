b2cc88fc315502bf00cb4e85997dcb86ce24163b
ULSSY <96038813+ULSSY@users.noreply.github.com>
2021-12-26 23:47:17 +0900

    2021

--- ml_app.py
+++ ml_app.py
@@ -6,7 +6,8 @@ import joblib
 
 def run_ml_app() :
     st.subheader('Machine Learning 예측')
-
+   
+    scaler_y = MinMaxScaler()
     df = pd.read_csv('data/Car_Purchasing_Data.csv', encoding='ISO-8859-1')
 
     # 1. 유저한테, 데이터를 입력받습니다.
@@ -28,12 +29,12 @@ def run_ml_app() :
     # 2-1. 신규데이터를 넘파이로 만든다.
     new_data = np.array([gender_number, age, salary, debt, worth])
     new_data = new_data.reshape(1, 5)
-
+    scaler_y = MinMaxScaler()
     # 2-2. 스케일러와 인공지능을 변수로 불러온다.
     scaler_X = joblib.load('data/scaler_X.pkl')
     scaler_y = joblib.load('data/scaler_y.pkl')
     regressor = joblib.load('data/regressor.pkl')
-
+     
     # 2-3. 신규데이터를 피쳐스케일링 한다.
     new_data = scaler_X.transform(new_data)
 
@@ -43,8 +44,8 @@ def run_ml_app() :
     # 2-5. 예측한 결과는, 다시 원래대로 복구해 줘야 한다. 
     st.write(y_pred)
 
-    # y_pred = scaler_y.inverse_transform(y_pred.reshape(-1,1))
-    # st.write(y_pred)
+    y_pred = scaler_y.inverse_transform(y_pred.reshape(-1,1))
+    st.write(y_pred)
 
     # # 3. 예측 결과를 웹 대시보드에 표시한다.
 
